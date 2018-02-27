eaa46e06eb5236e525536b531ecf9de4024eee46
Marco Castillo <marcocastillo2506@gmail.com>
2018-02-27 14:21:37 -0600

    Finished React authentication

--- ReactAuthentication/client/src/containers/SignUpPage.jsx
+++ ReactAuthentication/client/src/containers/SignUpPage.jsx
@@ -7,8 +7,8 @@ class SignUpPage extends React.Component {
   /**
    * Class constructor.
    */
-  constructor(props) {
-    super(props);
+  constructor(props, context) {
+    super(props, context);
 
     // set the initial component state
     this.state = {
@@ -25,8 +25,57 @@ class SignUpPage extends React.Component {
   }
 
   /**
-   * Change the user object.
+   * Process the form.
+   *
+   * @param {object} event - the JavaScript event object
+   */
+  processForm(event) {
+    // prevent default action. in this case, action is the form submission event
+    event.preventDefault();
 
+    // create a string for an HTTP body message
+    const name = encodeURIComponent(this.state.user.name);
+    const email = encodeURIComponent(this.state.user.email);
+    const password = encodeURIComponent(this.state.user.password);
+    const formData = `name=${name}&email=${email}&password=${password}`;
+
+    // create an AJAX request
+    const xhr = new XMLHttpRequest();
+    xhr.open('post', '/auth/signup');
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhr.responseType = 'json';
+    xhr.addEventListener('load', () => {
+      if (xhr.status === 200) {
+        // success
+
+        // change the component-container state
+        this.setState({
+          errors: {}
+        });
+
+        // set a message
+        localStorage.setItem('successMessage', xhr.response.message);
+
+        // make a redirect
+        this.context.router.replace('/login');
+      } else {
+        // failure
+
+        const errors = xhr.response.errors ? xhr.response.errors : {};
+        errors.summary = xhr.response.message;
+
+        this.setState({
+          errors
+        });
+      }
+    });
+    xhr.send(formData);
+  }
+
+  /**
+   * Change the user object.
+   *
+   * @param {object} event - the JavaScript event object
    */
   changeUser(event) {
     const field = event.target.name;
@@ -38,17 +87,9 @@ class SignUpPage extends React.Component {
     });
   }
 
-
-  processForm(event) {
-    // prevent default action. in this case, action is the form submission event
-    event.preventDefault();
-
-    console.log('name:', this.state.user.name);
-    console.log('email:', this.state.user.email);
-    console.log('password:', this.state.user.password);
-  }
-
-
+  /**
+   * Render the component.
+   */
   render() {
     return (
       <SignUpForm
@@ -61,5 +102,9 @@ class SignUpPage extends React.Component {
   }
 
 }
+
+SignUpPage.contextTypes = {
+  router: PropTypes.object.isRequired
+};
 
 export default SignUpPage;
